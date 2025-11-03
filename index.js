59cc61459042c15d631cf16f24e1f43ef60cb4a8
yo-dalo <kumaradarsh00572@gmail.com>
2025-11-03 12:43:31 +0530

    set host 0.0.0.0

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+
+const  remote_control= require("./routs/remote_control");
+
+
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+
+ app.use("/api", remote_control);
+ 
+
 
 // Routes
 app.get("/", (req, res) => {
@@ -8,6 +22,7 @@ app.get("/", (req, res) => {
 
 // Port
 const PORT = 5000;
-app.listen(PORT, () => {
+const HOSH = 5000;
+app.listen(PORT, HOST,() => {
   console.log(`Server started on port ${PORT}`);
 });
