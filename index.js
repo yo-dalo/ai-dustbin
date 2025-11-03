e93801f4c1a61d863a22d32ebb728684388f0d81
yo-dalo <kumaradarsh00572@gmail.com>
2025-11-03 12:44:37 +0530

    set host 0.0.0.0

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get("/", (req, res) => {
 
 // Port
 const PORT = 5000;
-const HOSH = 5000;
+const HOST = '0.0.0.0';
 app.listen(PORT, HOST,() => {
   console.log(`Server started on port ${PORT}`);
 });
